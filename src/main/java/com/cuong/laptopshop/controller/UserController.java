a521bd52ae4411ee698b93e4d6b5eeaf3256b680
Cuongdayne1811 <your-email@example.com>
2025-02-23 21:19:12 +0700

    het #49

--- src/main/java/com/cuong/laptopshop/controller/UserController.java
+++ src/main/java/com/cuong/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cuong.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,31 +9,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cuong.laptopshop.service.UserService;
 
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-// @Controller
-// public class UserController {
-//     @RequestMapping("/")
-    
-    
-//     public String getHomePage(){
-//         return "Hello from Controller";
-//     }
-// }
-@RestController
+@Controller
 public class UserController {
+
     private UserService userService ;
+
     
+
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
 
-    @GetMapping("/")
-    public String getHomePage(){
-        return this.userService.handleHello();
+
+
+    @RequestMapping("/")
+      
+    public String getHomePage(Model model){
+        String test = this.userService.handleHello();
+        model.addAttribute("test",test);
+        return "hello";
     }
 }
+// @RestController
+// public class UserController {
+
+//     // Sử dụng DI : dependency injection 
+//     private UserService userService ;
+    
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+
+//     @GetMapping("/")
+//     public String getHomePage(){
+//         return this.userService.handleHello();
+//     }
+// }
